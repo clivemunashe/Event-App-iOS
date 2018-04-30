8fe3b999e66d6c9f34f359c89557ad13a719eac7
clivemunashe <clyveshenje@gmail.com>
2018-04-30 02:16:04 +0100

    Add files via upload

---
+++ perfomtik/perfomtik/Article.swift
@@ -0,0 +1,23 @@
+//
+//  Article.swift
+//  perfomtik
+//
+//  Created by clive on 15/01/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+internal class Article : NSObject {
+    
+    internal var headline: String?
+    
+    internal var desc: String?
+    
+    internal var author: String?
+    
+    internal var url: String?
+    
+    internal var imageUrl: String?
+}
+
